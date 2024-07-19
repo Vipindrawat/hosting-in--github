b471016c85ea0eac061bf4587b2bc90cc9e98629
Vipindrawat <vipinrawat7500@gmail.com>
2024-07-19 17:37:05 +0530

    error/removed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-project/",
+  base: "/hosting-in-github/",
   plugins: [react()],
 })
